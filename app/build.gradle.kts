c0bc89f237f808cff4466c29ca38dcea0536f792
Michael Fleps <f.michi84.989@gmail.com>
2023-12-01 22:59:24 +0100

    2. SDK angepasst

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.mueddiswitze"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mueddiswitze"
