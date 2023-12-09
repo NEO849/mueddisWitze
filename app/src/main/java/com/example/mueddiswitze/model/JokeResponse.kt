244c2715c688f0a3ebde9c8ba0cad075d73efc10
Michael Fleps <f.michi84.989@gmail.com>
2023-12-09 03:47:03 +0100

    10. JokeFragment Logik definiert

--- app/src/main/java/com/example/mueddiswitze/model/JokeResponse.kt
+++ app/src/main/java/com/example/mueddiswitze/model/JokeResponse.kt
@@ -1,17 +1,5 @@
 package com.example.mueddiswitze.model
 
-class JokeResponse {
-
-    // Datenklasse, die die Struktur der API-Antwort repräsentiert
-    data class JokeResponse(
-        val error: Boolean,
-        val category: String,
-        val type: String,
-        val setup: String,
-        val delivery: String,
-        val flags: Map<String, Boolean>,
-        val id: Int,
-        val safe: Boolean,
-        val lang: String
-    )
-}
+data class JokeResponse(
+    val joke: String
+)
--- app/src/main/java/com/example/mueddiswitze/ui/JokeFragment.kt
+++ app/src/main/java/com/example/mueddiswitze/ui/JokeFragment.kt
@@ -1,19 +1,32 @@
 package com.example.mueddiswitze.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.mueddiswitze.R
+import com.example.mueddiswitze.viewmodel.JokeViewModel
 
-class JokeFragment : Fragment() {
+class JokeFragment : Fragment(R.layout.fragment_joke) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_joke, container, false)
+    private val viewModel: JokeViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.jokeResponse.observe(viewLifecycleOwner) { jokeResponse ->
+            // Setzt den Witz in die TextView
+            view.findViewById<TextView>(R.id.tvSet).text = jokeResponse.joke
+        }
+
+        // Witz abrufen, wenn das Fragment erstellt wird
+        viewModel.fetchJoke()
+
+        // Button-Click-Listener für den nächsten Witz
+        view.findViewById<Button>(R.id.btnNextJoke).setOnClickListener {
+            viewModel.fetchJoke() // Lädt den nächsten Witz
+        }
     }
 }
